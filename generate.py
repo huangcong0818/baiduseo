6f99121604b4f22d1c61e4c165deeb1ab1b09a35
huangcong0818 <15994762089@163.com>
2024-12-06 15:37:17 +0800

    Update generate.py

--- generate.py
+++ generate.py
@@ -4,8 +4,8 @@ import urllib
 import urllib.request
 import random
 
-site = 'https://blog.mhuig.top'
-sitemaps = ['/post-sitemap.xml','/page-sitemap.xml']
+site = 'https://pan.hyexz.com'
+sitemaps = ['/sitemap.xml']
 
 result = []
 bingData = {}
